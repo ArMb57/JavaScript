7c20bbe3d01a88c87a8f4ab4f1c9a84d1254a630
ArMb57 <matthieu@aready.io>
2024-01-03 13:50:46 +0100

    exos Js

--- Exos Js/Slider/script.js
+++ Exos Js/Slider/script.js
@@ -18,3 +18,100 @@
 // 4. N'oubliez pas de revenir à zéro quand on atteint l'index final et qu'on clic à droite, ou inversement.
   
 // C. Ajoutez du style à l'interface afin de terminer le projet.
+
+
+const slides = [...document.querySelectorAll('.slide')];
+
+const btns = [...document.querySelectorAll('.direction-btn')];
+
+
+const data = {
+    slideOut:0, // Diapo courante 
+    slideIn:0, // Diapo suivante
+    direction:0
+}
+
+
+btns.forEach( btn => btn.addEventListener('click',sliding));
+
+function sliding(event){
+    slideOut()
+
+    // définir la direction du slide
+    data.direction = event.target.className.includes('right') ? 1 : -1;
+
+    // Trouver l'indice du slide courant 
+    data.slideOut = slides.findIndex(slide => slide.classList.contains('active'));
+    
+    // Trouver l'index du slide suivant
+    if(data.slideOut + data.direction > slides.length - 1){
+        data.slideIn = 0;
+        console.log("premier if ");
+    }
+    else if (data.slideOut + data.direction < 0){
+        data.slideIn = slides.length - 1;
+        console.log("else if");
+    }else{
+        data.slideIn = data.slideOut + data.direction;
+        console.log("else");
+    }
+    
+    console.log(" la diapo courante est à l'index " + data.slideOut + " La diapo suivante est à l'index " + data.slideIn);
+    console.log(" la direction est : " + data.direction);
+    
+    console.log(data)
+
+}
+
+// Fonction pour appliquer les props de transition
+function slideTransition(obj){
+for( let prop in obj.props){
+    obj.ele.style[prop] = obj.props[prop];  
+}
+
+}
+
+function slideOut(){
+
+    // Appliquer les props pour afficher la nouvelle diapo 
+    slideTransition({
+        ele: slides[data.slideIn],
+        props: {
+            transform:`translateX(${data.direction < 0 ? "100%" : "-100%"})`,
+         
+        }
+    })
+
+    // Appliquer les props pour faire sortir l'ancienne diapo
+    
+    slideTransition({
+        ele: slides[data.slideOut],
+        props: {
+            transform:`translateX(${data.direction < 0 ? "-100%" : "100%"})`,
+            transition: "transform 0.5s ease-in-out"
+            
+        }
+    })
+    slides[data.slideOut].addEventListener('transitionend', slideIn);
+}
+
+
+function slideIn(event){
+ 
+    slideTransition({
+        ele: slides[data.slideIn],
+        props: {
+            transform:`translateX(0%)`,
+            
+        }
+    })
+
+    
+    // Ajouter la classe active sur la nouvelle diapo
+    slides[data.slideIn].classList.add('active');
+    
+    // Retirer la classe active sur l'ancienne diapo
+    slides[data.slideOut].classList.remove('active');
+    // slides[data.slideOut].style.display = "none";
+    event.target.removeEventListener('transitionend', slideIn);
+}
--- Exos Js/Validation/script.js
+++ Exos Js/Validation/script.js
@@ -20,3 +20,148 @@
 // - Enfin envoyez un .alert("données envoyées") si on appuie sur le bouton "Création du compte" en ayant passé tous les tets.
   
 //  C. Ajoutez du style à l'interface afin de terminer le projet.
+
+// Sélection 
+const validationIcons = document.querySelectorAll('.icone-verif');
+const validationTexts = document.querySelectorAll('.error-msg');
+const inputs = document.querySelectorAll('input');
+
+// Fonction d'affichage de la validation
+function displayValidation(validation, index){
+    if(validation){
+        validationIcons[index].style.display = 'inline';
+        validationIcons[index].src="ressources/check.svg";
+       if(validationTexts[index]) validationTexts[index].style.display = 'none';
+    }else{
+        validationIcons[index].style.display = 'inline';
+        validationIcons[index].src="ressources/error.svg";
+        if(validationTexts[index])validationTexts[index].style.display = 'block';
+    }
+}
+
+
+// Validation de l'utilisateur
+const userInput = inputs[0];
+userInput.addEventListener('input',userValiation);
+
+function userValiation(){
+if(userInput.value.length >= 3){
+        displayValidation(true, 0)
+}else{
+        displayValidation(false, 0)
+}
+}
+
+
+// Validation de l'email
+const mailInput = inputs[1];
+mailInput.addEventListener('input',mailValidation);
+
+const regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+function mailValidation(){
+
+    if(regexMail.test(mailInput.value)){
+        displayValidation(true, 1)
+    }else{
+        displayValidation(false, 1)
+    }
+
+ }
+
+// Validation du mot de passe
+const pwdInput = inputs[2];
+pwdInput.addEventListener('input',pswdValidation);
+
+const pwdVerif = {
+    length : false,
+    symbol : false,
+    number : false
+}
+
+const regexList ={
+    symbol: /[^a-zA-Z0-9\s]/,
+    number: /\d/
+}
+
+function pswdValidation(){
+let countVerif = 0;
+
+    // Vérification de la longueur
+if(pwdInput.value.length >= 6){
+pwdVerif.length = true;
+countVerif++;
+}
+
+// Vérification des symboles et des chiffres
+for(const prop in regexList){
+    if(regexList[prop].test(pwdInput.value)){
+        pwdVerif[prop] = true;
+        countVerif++;
+    }
+}
+
+// vérification si le champ est valide
+if(countVerif !== 3){
+    displayValidation(false, 2)
+}else{
+    displayValidation(true, 2)
+}
+
+
+pwdPower();
+}
+
+// Vérification de la puissance du mot de passe 
+
+const lines = document.querySelectorAll('.lines div');
+const pwdConfirm = inputs[3];
+function pwdPower(){
+ const pwdLength = pwdInput.value.length;
+
+ lines.forEach((el) => {
+        el.style.display = 'none';
+ });
+
+    if(!pwdLength){
+        return;
+    }
+    else if(pwdLength > 9 && pwdVerif.symbol && pwdVerif.number) {
+        console.log('fort');
+        lines[0].style.display = 'block';
+        lines[1].style.display = 'block';
+        lines[2].style.display = 'block';
+
+    }else if (pwdLength > 6 && pwdVerif.symbol && pwdVerif.number){
+        console.log('moyen');
+        lines[0].style.display = 'block';
+        lines[1].style.display = 'block';
+    }else{
+        console.log('faible');
+        lines[0].style.display = 'block';
+    }
+
+    if(pwdConfirm.value.length !== 0){
+    confirmValidation()
+    }
+}
+
+
+// Vérification de la confirmation du mot de passe
+
+
+pwdConfirm.addEventListener('input', confirmValidation);
+
+function confirmValidation(){
+    const confirmedpwd = pwdConfirm.value;
+
+    //  Vérifier si les deux champs si vides
+    if(!confirmedpwd && ! pwdInput.value){
+        return;
+    }
+    if(confirmedpwd === pwdInput.value){
+        displayValidation(true, 3)
+    }else{
+        displayValidation(false, 3)
+    }
+
+}

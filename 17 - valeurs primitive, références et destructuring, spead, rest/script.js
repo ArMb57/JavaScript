dc96b96e1e5b5fefa0116429624b121a28886281
ArMb57 <matthieu@aready.io>
2024-01-15 11:12:00 +0100

    Objets

---
+++ 17 - valeurs primitive, références et destructuring, spead, rest/script.js
@@ -0,0 +1,113 @@
+// // Valeur primitive 
+// let a = 'abc';
+// let b = a;
+// a = 'def';
+// console.log(a)
+// console.log(b); // affiche 'abc'
+
+// // Valeur de référence
+// let obj1 = {str:"abc"};
+// let obj2 = obj1; // obj2 est une référence vers obj1
+// obj1.str = "def";
+// console.log(obj2)
+
+// Destructuring 
+// Permet d'extraire des valeurs d'un tableau ou d'un objet et les assigner à des variables distinctes.
+
+
+// destructuring sur un tableau 
+// let infos = ['Jean', 'Dupont', 35];
+
+// affectation sans destructuring
+// let nom = infos[0];
+// let prenom = infos[1];
+// let age = infos[2];
+
+// affectation avec destructuring
+// let [nom, prenom, age] = infos;
+// console.log(nom, prenom, age);
+
+
+// destructuring sur un objet 
+// let user = {
+//     nom: 'Jean',
+//     prenom: 'Dupont',
+//     age: 35
+// };
+
+// affectation sans destructuring
+// let nom = user.nom;
+// let prenom = user.prenom;
+// let age = user.age;
+
+// affectation avec destructuring
+// let {nom, prenom, age} = user;
+// console.log(nom, prenom, age);
+
+
+// question comment stocker cette obj dans des var distinctes
+// let user = {
+//     nom: 'Jean',
+//     prenom: 'Dupont',
+//     age: 35, 
+//     adresse: {
+//         rue: 'rue de la paix',
+//         ville: 'Paris'
+//     }
+// };
+
+
+
+// Décomposition ou spread operator 
+//Combiner plusieurs tableaux ou objets ou créer une copie superficielle d'un objet.
+
+// let fruits = ['pomme', 'poire', 'banane'];
+// let aliments = ['tomate', 'carotte']; 
+
+// // Exemple 1
+// let fruits2 = fruits; // copie sans spread 
+// let fruits3 = [...fruits]; // copie avec spread
+
+// // Exemple 2 
+// let total = [...fruits, ...aliments]; // combinaison de tableaux avec spread
+// let total2 = fruits.concat(aliments); // combinaison de tableaux sans spread
+// console.log(total2);
+
+// // Exemple 3
+
+// let str = "Bonjour!";
+// let str2 = [...str]; // décomposition d'une chaîne de caractères
+// let str3 = str.split(''); // décomposition d'une chaîne de caractères
+// console.log(str3);
+
+
+// exemple copie superficielle d'un objet
+// let obj1 = {str:"abc"};
+// let obj2 = {...obj1}; 
+// obj1.str = "def";
+// console.log(obj2) // affiche {str: "abc"}
+
+
+// rest parameter 
+// permet de passer un nombre variable d'arguments à une fonction sous forme de tableau.
+
+// function add( a, b ){
+//     return a + b;
+// }
+
+// console.log(add(1, 2)); 
+
+// somme de nombres avec le rest parameter
+function sum(...nb){
+    let total = 0;
+    for(let n of nb){
+        total += n;
+    }
+    return total;
+}
+console.log(sum(1, 2, 3, 4, 5)); // affiche 15
+
+function sum(...nb){
+    return nb.reduce((total, n) => total + n);
+}
+console.log(sum(1, 2, 3, 4, 5)); // affiche 15
---
+++ 18 - Les objets/script.js
@@ -0,0 +1,144 @@
+// Introduction au mot clé this 
+
+// let user = {
+//     name: "John",
+//     age: 30,
+
+//     sePresenter(){
+//         // this fait au contexte appelant CAD l'objet dans lequel il est appelé
+//         console.log(`Bonjour, je m'appelle ${this.age}`);
+//         // Manière equivalente de faire
+//         console.log(`Bonjour, je m'appelle ${user.name}`)
+//     }
+// };
+
+// user.sePresenter();
+
+
+// L'objet Set(), permet de stocker des valeurs unique de n'importe quel type, mais ne stock que des valeurs ( pas d'indices)
+
+// Exemple 1 : supprimer les valeurs dupliquées d'un tableau
+// let nombres = [10, 45, 75, 10, 24, 45];
+// let monSet = new Set(nombres);
+// console.log(monSet);
+
+// QUESTION : Comment faire pour supprimer les valeurs dupliquer sans utiliser un Set() ? 
+
+
+
+// Exemple 2 : utilisations des méthodes propres au Set()
+
+// let monSet2 = new Set();
+
+// // Ajouter des valeurs dans un Set
+// monSet2.add("Hello");
+// monSet2.add("Bonjour");
+
+// // Supprimer des valeurs dans un Set
+// // monSet2.delete("Bonjour");
+
+// // Vérifier si une valeur existe dans un Set
+// console.log(monSet2.has("Hello"));
+
+// // Afficher le nombre de valeurs dans un Set
+// console.log(monSet2.size);
+
+// // tout supprimer dans un Set
+// monSet2.clear();
+
+// console.log(monSet2);
+
+
+
+// Map() est un objet qui stock des paires clé/valeur. Contrairement aux objets, les clés peuvent être de n'importe quel type (string, number, boolean, object, function, etc...)
+
+// Exemple 1 comparaison renvoyer longueur d'un objet littéral et d'un Map()
+// let obj = {
+//     name: "John",
+//     age: 30
+// };
+// // Renvoyer le nb d'éléments dans un objet littéral
+// console.log(Object.keys(obj).length);
+
+// // transformer un objet littéral en Map()
+// let myMap = new Map(Object.entries(obj));
+// // Afficher le nombre d'éléments dans un Map()
+// console.log(myMap.size);
+
+// Exemple 2: utilisation des méthodes propres au Map()
+
+// let users = new Map();
+
+// // Envoyer des données dans le Map()
+// users.set("Mark zuckerberg", {
+//     email: "mark@facebook.com",
+//     poste: "PDG"
+// });
+
+// users.set("Elon Musk", {
+//     email: "elon@musk.com",
+//     poste: "sauver le monde"
+// });
+
+// // Récupérer les données dans le Map()
+// console.log(users.get("Mark zuckerberg"));
+
+// // Vérifier si une clé existe dans le Map()
+// console.log(users.has("Mark zuckerberg"));
+
+// // Supprimer une clé dans le Map()
+// users.delete("Mark zuckerberg");
+// console.log(users)
+
+
+// // Exemple 3 : transformer un tableau en Map()
+// // !!! Il faut que le tableau soit un tableau de tableaux
+// let myArr = [
+//     ["1", "HTML"],
+//     ["2", "CSS"],
+//     ["3", "JS"],
+//     ["4", "PHP"]
+// ];
+// let map2 = new Map(myArr);
+// console.log(map2);
+
+
+// Objet Date()
+// Récupérer la date au format UTC 
+// let date = new Date();
+
+// Récupérer le timestamp de la date actuelle 
+// let timestamp = date.getTime();
+
+
+// // Récupérer la date local 
+// let localDate = date.toLocaleDateString('fr-FR');
+// // Récupérer l'heure local
+// let localTime = date.toLocaleTimeString('fr-FR');
+// console.log(localTime);
+
+// Affichage de date configuré 
+// let options = {year: "numeric", month: "long", day: "numeric", hour:'2-digit', minute:'2-digit', second:'2-digit'};
+// let dateConfig = date.toLocaleDateString('fr-FR', options);
+// console.log(dateConfig)
+
+
+
+// QUESTION : comment récupérer l'heure sur un autre fuseau horaire ?
+
+
+
+
+
+// Objet Math()
+let x = 4.3;
+let y = 5.6;
+console.log(Math.abs(x)); // renvoie 4.3
+console.log(Math.ceil(x)); // renvoie 5
+console.log(Math.floor(x)); // renvoie 4 
+console.log(Math.round(x)); // renvoie 4
+console.log(Math.max(x, y)); // renvoie 5.6
+console.log(Math.min(x, y)); // renvoie 5.6
+console.log(Math.pow(x, 2)); // renvoie 18.49
+console.log(Math.sqrt(x)); // renvoie 2.07
+console.log(Math.random());
